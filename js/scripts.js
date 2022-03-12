24a85e77a4a65f6f5891b8ae1b3a4999499320e4
Patricia McPhee <hi@patriciamcphee.com>
2022-03-12 12:32:30 -0800

    fixed errors after testing in repl console

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 let pokemonList = [
-    {name:'Ivysaur', height:'1.0 m', type:'Grass'}
-    {name:'Charmander', height:'0.6 m', type:'Fire'}
-    {name:'Stonjourner', height:'2.5 m', type:'Rock'}
-    {name:'Spectrier', height:'2.0 m', type:'Ghost'}
-    {name:'Copperajah', height:'3.0 m', type:'Steel'}
-    {name:'Eiscue', height:'1.4 m', type:'Ice'}
-]; //array list
+    1,
+    {name:'Ivysaur', height:'1.0 m', type:'Grass'},
+    2,
+    {name:'Charmander', height:'0.6 m', type:'Fire'},
+    3,
+    {name:'Stonjourner', height:'2.5 m', type:'Rock'},
+    4,
+    {name:'Spectrier', height:'2.0 m', type:'Ghost'},
+    5,
+    {name:'Copperajah', height:'3.0 m', type:'Steel'},
+    6,
+    {name:'Eiscue', height:'1.4 m', type:'Ice'},
+  ];
+  
+  
+  console.log(pokemonList[1].name);
+  

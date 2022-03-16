effc1d51e62ffe736c67d594f8912cef97337b68
Patricia McPhee <hi@patriciamcphee.com>
2022-03-15 17:42:13 -0700

    added a for loop with if-then-else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,47 @@
 let pokemonList = [
-    {name:'Ivysaur', height:'1.0 m', type:'Grass'},
-    {name:'Charmander', height:'0.6 m', type:'Fire'},
-    {name:'Stonjourner', height:'2.5 m', type:'Rock'},
-    {name:'Spectrier', height:'2.0 m', type:'Ghost'},
-    {name:'Copperajah', height:'3.0 m', type:'Steel'},
-    {name:'Eiscue', height:'1.4 m', type:'Ice'},
-];
+    {
+      name: "Ivysaur",
+      height: 1.0,
+      type: ["Grass"]
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      type: ["Fire"]
+    },
+    {
+      name: "Stonjourner",
+      height: 2.5,
+      type: ["Rock"]
+    },
+    {
+      name: "Spectrier",
+      height: 2.0,
+      tpe: ["Ghost"]
+    },
+    {
+      name: "Copperajah",
+      height: 3.0,
+      type: ["Steel"]
+    },
+    {
+      name: "Eiscue",
+      height: 1.4,
+      type: ["Ice"]
+    }
+  ];
   
-console.log(pokemonList[3].name);
+  for (let x=0; x < pokemonList.length; x++){
+    if (pokemonList[x].height < 1) { //any pokemon smaller than 1 m
+      document.write(pokemonList[x].name + ' (height: ' + pokemonList[x].height + ') - Oh my! you look itty bitty!');
+      document.write("<br />");
+    } else if (pokemonList[x].height > 1.4) { //any Pokemon larger than 1.4 m
+      document.write(pokemonList[x].name + ' (height: ' + pokemonList[x].height + ') - Whoa there big fella!');
+      document.write("<br />");
+    } else {
+          document.write(pokemonList[x].name + ' (height: ' + pokemonList[x].height + ')');
+      document.write("<br />");
+    }
+  }
+  
+  

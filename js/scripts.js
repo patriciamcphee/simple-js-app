3f51b3aeb2cd23326991a26c9ae9cb1508e10ca4
Patricia McPhee <hi@patriciamcphee.com>
2022-04-03 15:29:17 -0700

    manipulate DOM practice exercise

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,42 @@ let pokemonRepository = (function () {
     //original Pokemon list
     let pokemonList = [
     {
-        name: "Ivysaur",
+        name: 'Ivysaur',
         height: 1.0,
-        type: ["Grass"]
+        type: ['Grass'],
+        description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
     },
     {
-        name: "Charmander",
+        name: 'Charmander',
         height: 0.6,
-        type: ["Fire"]
+        type: ['Fire'],
+        description: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
     },
     {
-        name: "Stonjourner",
+        name: 'Stonjourner',
         height: 2.5,
-        type: ["Rock"]
+        type: ['Rock'],
+        description: 'It stands in grasslands, watching the sun\’s descent from zenith to horizon. This Pokémon has a talent for delivering dynamic kicks.',
     },
     {
-        name: "Spectrier",
+        name: 'Spectrier',
         height: 2.0,
-        tpe: ["Ghost"]
+        tpe: ['Ghost'],
+        description: 'It probes its surroundings with all its senses save one—it doesn\’t use its sense of sight. Spectrier\’s kicks are said to separate soul from body.',
     },
     {
-        name: "Copperajah",
+        name: 'Copperajah',
         height: 3.0,
-        type: ["Steel"]
+        type: ['Steel'],
+        description: 'They came over from another region long ago and worked together with humans. Their green skin is resistant to water.',
     },
     {
-        name: "Eiscue",
+        name: 'Eiscue',
         height: 1.4,
-        type: ["Ice"]
+        type: ['Ice'],
+        description: 'It drifted in on the flow of ocean waters from a frigid place. It keeps its head iced constantly to make sure it stays nice and cold.',
     }
-    ];
+  ];
   
   function add(pokemon) {
      if (
@@ -50,26 +56,31 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
-function addItem(pokemon) {
+
+
+  function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
-    let item = document.createElement('p');
-    if (pokemon.height <= 1) {
-      document.write(pokemon.name + ' (height: ' + pokemon.height + ' m) - Oh my! you look itty bitty!');
-      document.write('<br />');
-    } else if (pokemon.height >= 3) {
-      document.write(pokemon.name + ' (height: ' + pokemon.height + ' m) - Whoa there big fella!');
-      document.write('<br />');
-    } else {
-      document.write(pokemon.name + ' (height: ' + pokemon.height + ' m)');
-      document.write('<br />');
-    };
-    pokemonList.appendChild(item);
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    // Reveals object details when button is clicked
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    })
+  }
+
+  function showDetails(pokemon) {
+    console.clear();
+    console.log(pokemon);
   }
 
   return {
     add: add,
     getAll: getAll,
-    addItem: addItem
+    addListItem: addListItem
   };  
 
 })();
@@ -79,19 +90,22 @@ function addItem(pokemon) {
 pokemonRepository.add({
   name: 'Tentacool',
   height: 2.9,
-  types: ['Poison', 'Water']
+  types: ['Poison', 'Water'],
+  description: 'Tentacool is not a particularly strong swimmer. It drifts across the surface of shallow seas as it searches for prey.',
 });
 
 pokemonRepository.add({
   name: 'Butterfree',
   height: 3.5,
-  types: ['Bug', 'Flying']
+  types: ['Bug', 'Flying'],
+  description: 'In battle, it flaps its wings at great speed to release highly toxic dust into the air.',
 });
 
 pokemonRepository.add({
   name: 'Metapod',
   height: 2.3,
-  types: ['Bug']
+  types: ['Bug'],
+  description: 'It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack.',
 });
 
 
@@ -100,5 +114,9 @@ console.log(pokemonRepository.getAll());
 
 // add item to Pokemon repository
 pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addItem(pokemon);
+  pokemonRepository.addListItem(pokemon);
 });
+
+
+
+

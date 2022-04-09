a17a86c1fbe6b09532b5ef85c241a3dbc2c8da6f
Patricia McPhee <hi@patriciamcphee.com>
2022-04-09 11:14:53 -0700

    add modal for pokemon buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,30 @@
-let pokemonRepository = (function () {
-    //original Pokemon list
-    let pokemonList = [
-    {
-        name: 'Ivysaur',
-        height: 1.0,
-        type: ['Grass'],
-        description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        type: ['Fire'],
-        description: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
-    },
-    {
-        name: 'Stonjourner',
-        height: 2.5,
-        type: ['Rock'],
-        description: 'It stands in grasslands, watching the sun\’s descent from zenith to horizon. This Pokémon has a talent for delivering dynamic kicks.',
-    },
-    {
-        name: 'Spectrier',
-        height: 2.0,
-        tpe: ['Ghost'],
-        description: 'It probes its surroundings with all its senses save one—it doesn\’t use its sense of sight. Spectrier\’s kicks are said to separate soul from body.',
-    },
-    {
-        name: 'Copperajah',
-        height: 3.0,
-        type: ['Steel'],
-        description: 'They came over from another region long ago and worked together with humans. Their green skin is resistant to water.',
-    },
-    {
-        name: 'Eiscue',
-        height: 1.4,
-        type: ['Ice'],
-        description: 'It drifted in on the flow of ocean waters from a frigid place. It keeps its head iced constantly to make sure it stays nice and cold.',
-    }
-  ];
-  
+let pokemonRepository = (function() {
+
+  //Pokemon List Array with nested Objects
+  let pokemonList = [];
+  // API link for the Pokemon repository
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
+  // Modal container
+  let modalContainer = document.querySelector('#modal-container');
+
+  // Public functions
   function add(pokemon) {
-     if (
+    if (
       typeof pokemon === 'object' &&
       'name' in pokemon &&
-      'height' in pokemon &&
-      'types' in pokemon
+      'detailsUrl' in pokemon
+      //'height' in pokemon &&
+      //'types' in pokemon
     ) {
-    pokemonList.push(pokemon);
-  } else {
-    console.log('Sorry Ash, the Pokémon isn\'t correct!');
-  }
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Pokémon is not correct');
+    }
   }
 
   function getAll() {
     return pokemonList;
   }
-
-
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -66,56 +34,127 @@ let pokemonRepository = (function () {
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    // Event listener - Reveals object details when button is clicked
+    // Event Listener added that reveals object details when button is clicked
     button.addEventListener('click', function() {
       showDetails(pokemon);
     })
   }
 
-  function showDetails(pokemon) {
-    //clear console upon each click
-    console.clear();
-    console.log(pokemon.name);
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    });
   }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = [];
+      details.types.forEach(function(detail) {
+        item.types.push(' ' + detail.type.name);
+      })
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function() {
+      showModal(item);
+      console.log(item)
+    })
+  }
+
+  // showModal function
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    // hideModal close button
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: '  + '\n' + pokemon.height + 'm';
+
+    let typesElement = document.createElement('p');
+    typesElement.innerText = 'Types: '  + '\n' + pokemon.types;
+
+    // Modal image container
+    let container = document.querySelector('.pokemon-image-details-container');
+    let pokemonImage = document.createElement('img');
+    pokemonImage.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(typesElement);
+    modal.appendChild(pokemonImage);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  // hideModal function
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  // hideModal Esc key
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  // hideModal click outside modal
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
-  };  
-
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
+    showModal: showModal,
+    hideModal: hideModal
+  };
 })();
 
-
-// Add Pokemon
-pokemonRepository.add({
-  name: 'Tentacool',
-  height: 2.9,
-  types: ['Poison', 'Water'],
-  description: 'Tentacool is not a particularly strong swimmer. It drifts across the surface of shallow seas as it searches for prey.',
+// Add Pokemon function
+pokemonRepository.loadList().then(function() {
+  // forEach() Loop with DOM manipulation
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
-
-pokemonRepository.add({
-  name: 'Butterfree',
-  height: 3.5,
-  types: ['Bug', 'Flying'],
-  description: 'In battle, it flaps its wings at great speed to release highly toxic dust into the air.',
-});
-
-pokemonRepository.add({
-  name: 'Metapod',
-  height: 2.3,
-  types: ['Bug'],
-  description: 'It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack.',
-});
-
-
-
-// add item to Pokemon repository
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
-
-
-
-
